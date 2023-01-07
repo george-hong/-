04556c13d3130c635ff8093886a80b8a48940d3a
george-hong <hcj147@vip.qq.com>
2023-01-07 22:43:20 +0800

    refactor: 支持上传文件到服务器

--- node/router/worker/index.js
+++ node/router/worker/index.js
@@ -1,14 +1,41 @@
 import express from 'express';
-import multiparty from 'multiparty';
+import formidable from 'formidable';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
 
 const router = new express.Router();
+const arrayBufferMapping = {};
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-router.post('/upload', function(req, res) {
-  const form = new multiparty.Form();
+const getType = (value) => {
+  return ({}).toString.apply(value);
+}
+
+router.post('/upload', function(req, res, next) {
+  const form = formidable({
+    uploadDir: path.join(__dirname, '../../file'),
+    filename(name, ext, part, form) {
+      return part.originalFilename;
+    }
+  });
   form.parse(req, function (err, fields, files) {
+    if (err) {
+      console.log('err', err);
+      next(err);
+    }
+    const { total, index, id } = fields;
 
-    res.send('upload');
+    console.log('files', files)
+    // const { blob } = files;
+    // if (!arrayBufferMapping[id]) arrayBufferMapping[id] = {};
+    // arrayBufferMapping[id][index] = blob[0];
+    //
+    // const isFileLoaded = Array.apply(undefined, { length: total }).every((item, index) => !!arrayBufferMapping[id][index]);
+    // if (isFileLoaded) {
+    //   const file = new Blob(arrayBufferMapping[id], { type: 'image/png' });
+    // }
+    res.send('success');
   });
 });
 
