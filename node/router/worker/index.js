a74f318191eb6e44092a69d3ac8432fd4dee648b
george-hong <hcj147@vip.qq.com>
2023-01-09 23:54:39 +0800

    refactor: 暂存文件

--- node/router/worker/index.js
+++ node/router/worker/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import formidable from 'formidable';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import fs, { stat } from 'fs';
 
 
 const router = new express.Router();
@@ -10,6 +11,37 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const getType = (value) => {
   return ({}).toString.apply(value);
+}
+
+class FileHelper {
+  // file name rule `${id}.${segmentIndex}.${totalSegments}.{format}`
+  static checkSegmentsAllUploaded(fileName) {
+    return new Promise((resolve, reject) => {
+      const [id, index, total, extension] = fileName.split('.');
+      const fileStatus = Array
+        .apply(undefined, { length: total })
+        .map(() => ({ checked: false }));
+      let finished = false;
+      let hasError = false;
+      const onSegmentChecked = () => {
+        if (finished) return;
+        finished = fileStatus.every((stateInfo) => {
+          hasError = stateInfo.status === false;
+          return stateInfo.checked;
+        });
+        if (hasError) finished = true;
+        if (finished) hasError ? reject() : resolve();
+      };
+      fileStatus.forEach((status, index) => {
+        stat(path.join(__dirname, `../../file/${fileName}`), (err, stats) => {
+          fileStatus[index].status = true;
+          fileStatus[index].checked = true;
+          onSegmentChecked();
+        });
+      });
+
+    });
+  }
 }
 
 router.post('/upload', function(req, res, next) {
@@ -26,7 +58,16 @@ router.post('/upload', function(req, res, next) {
     }
     const { total, index, id } = fields;
 
-    console.log('files', files)
+    // console.log('files', files)
+
+    FileHelper.checkSegmentsAllUploaded(files.blob.newFilename)
+      .then(result => {
+        console.log('success');
+      })
+      .catch(err => {
+        console.log('fail');
+      });
+
     // const { blob } = files;
     // if (!arrayBufferMapping[id]) arrayBufferMapping[id] = {};
     // arrayBufferMapping[id][index] = blob[0];
